b8d68375b03ea314604f84443a2b21f88ce5b6dd
216037150 <siyabongazungu95@gmail.com>
2025-09-15 10:52:19 +0200

    loops

---
+++ src/main/java/org/javaproblems/PracticeMakePerfect/WhlieLoop.java
@@ -0,0 +1,11 @@
+package org.javaproblems.PracticeMakePerfect;
+
+public class WhlieLoop {
+    public static void main(String[] args) {
+        int n = 1;
+        while(n<=4){
+            System.out.println("Hello There " + n);
+            n++;
+        }
+    }
+}
