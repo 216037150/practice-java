d8a3d3316e45f3c9228825e1b6eb320b270cfff5
216037150 <siyabongazungu95@gmail.com>
2025-09-15 10:58:37 +0200

    Ternary operator

---
+++ src/main/java/org/javaproblems/PracticeMakePerfect/TenaryOparator.java
@@ -0,0 +1,10 @@
+package org.javaproblems.PracticeMakePerfect;
+
+public class TenaryOparator {
+    public static void main(String[] args) {
+        int n = 3;
+        String result;
+        result = (n%2 == 0) ? "Even":"Odd";
+        System.out.println(result);
+    }
+}
